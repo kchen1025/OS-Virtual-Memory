24da8132c53cb9b07d4a2c1daa07394f1e979714
Kevin Chen <kchen1025@gmail.com>
2016-03-05 05:57:47 -0800

    added a driver for the project without the TLB

--- src/project3_143a/Driver.java
+++ src/project3_143a/Driver.java
@@ -1,7 +1,74 @@
 package project3_143a;
 
-public class Driver {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Driver {	
+	
 	public static void main(String args[]){
-		System.out.println("lols");
+		BufferedReader br = null;
+
+		PhysicalMemory pm = new PhysicalMemory();
+		pm.init();
+		
+		String inputFile1 = "file1.txt";
+		String inputFile2 = "file2.txt";
+		String outputFile = "498592231.txt";
+		
+		try {
+			//creating ouptut file 
+			File file2 = new File(outputFile);
+	        if (!file2.exists()) {
+				file2.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(file2.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			br = new BufferedReader(new FileReader(inputFile1));
+
+			String[] firstLine;
+			String[] secondLine;
+
+			firstLine = br.readLine().split(" ");
+			secondLine = br.readLine().split(" ");
+			
+			//initiatlize the segment tables for line 1 
+			for(int i = 0; i < firstLine.length; i+=2){
+				pm.setST(Integer.parseInt(firstLine[i]), Integer.parseInt(firstLine[i+1]));
+			}
+			
+			//initalize the page tables for all of line 2
+			for(int i = 0; i < secondLine.length; i+=3){
+				pm.setPT(Integer.parseInt(secondLine[i]), Integer.parseInt(secondLine[i+1]), Integer.parseInt(secondLine[i+2]));
+			}
+			
+			
+			//read the second input file for the virtual addresses
+			br = new BufferedReader(new FileReader(inputFile2));
+			firstLine = br.readLine().split(" ");
+			
+			for(int i = 0; i < firstLine.length; i+=2){
+				bw.write(pm.addressTranslation(Integer.parseInt(firstLine[i]),Integer.parseInt(firstLine[i+1]))+" ");
+			}
+			
+			
+			bw.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
 	}
 }
